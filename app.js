52d4aea36c2ebdb6f9cea26ec461e1670060d60e
Eli Shagam <eli.shagam@gmail.com>
2022-06-23 18:35:51 +0300

    save temp befor axios for splits

--- app.js
+++ app.js
@@ -29,7 +29,9 @@ app.listen(port, (err) => {
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', (req, res) => {
   console.log ("entry")
-  const dat = corsBypass (req, res)
+
+
+  // const dat = corsBypass (req, res)
   // console.log (dat.length)
   res.send(dat)
 })
@@ -44,22 +46,24 @@ const corsBypass = async (req, res) => {
   // return res.status(200).json(dat.data)
 }
 
-module.exports = {
-  corsBypass
-}
+// module.exports = {
+//   corsBypass
+// }
 
 app.get('/splits', (req, res) => {
-  res.send(req.query.stock, req.url)
+
   const url = "https://www.stocksplithistory.com/?symbol=" + req.query.stock;
   const options = {
     "method": "GET",
   };
 
+  res.send(req.query.stock, req.url)
+})
 
   // // Pattern pattern = Pattern.compile("#CCCCCC\">(\\d\\d)/(\\d\\d)/(\\d\\d\\d\\d)</TD><TD align=\"center\" style=\"padding: 4px; border-bottom: 1px solid #CCCCCC\">(\\d*) for (\\d*)");
 
 //         const pattern = "/#CCCCCC\">(\\d\\d)/(\\d\\d)/(\\d\\d\\d\\d)</TD><TD align=\"center\" style=\"padding: 4px; border-bottom: 1px solid #CCCCCC\">(\\d*) for (\\d*)/g";
 //         }
 //   )
-})
+
 
