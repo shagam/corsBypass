be5d3bac83575abdfeb588ed9b916972406edadf
Eli Shagam <eli.shagam@gmail.com>
2022-06-26 11:19:03 +0300

    Comment experiments, build split json

--- app.js
+++ app.js
@@ -40,35 +40,36 @@ app.use (
 
 app.get('/splits', (req, res) => {
   
-  const iCalContent = " Summery=blablabla summery=bla"
-  var rx = /summery=(\w*)/i;
-  var arr = rx.exec(iCalContent);
-  console.log ('substring2 ',arr);
+  // const iCalContent = " Summery=blablabla summery=bla"
+  // var rx = /summery=(\w*)/i;
+  // var arr = rx.exec(iCalContent);
+  // console.log ('substring2 ',arr);
 
   
-const input = '[2021-05-29] Version 2.24.9  13.13.14';
-const regex = /(\d+)\.(\d+)\.(\d+)/g;
-let x = regex.exec(input);
-console.log('test4', 'substring3 ', x)
+// const input = '[2021-05-29] Version 2.24.9  13.13.14';
+// const regex = /(\d+)\.(\d+)\.(\d+)/g;
+// let x = regex.exec(input);
+// console.log('test4', 'substring3 ', x)
 
-var regEx = new RegExp('([0-9]+ (cat|fish))','g');
-var sampleString1="1 cat and 2 fish";
-var result = sampleString1.match(regEx);
-console.log('test5', JSON.stringify(result));
+// var regEx = new RegExp('([0-9]+ (cat|fish))','g');
+// var sampleString1="1 cat and 2 fish";
+// var result = sampleString1.match(regEx);
+// console.log('test5', JSON.stringify(result));
 // ["1 cat","2 fish"]
 
-console.log('test6')
-var reg = new RegExp('[0-9]+ (cat|fish)','g'), sampleString="1 cat and 2 fish";
-while ((result = reg.exec(sampleString)) !== null) {
-    console.dir(JSON.stringify(result))
-};
+// console.log('test6')
+// var reg = new RegExp('[0-9]+ (cat|fish)','g'), sampleString="1 cat and 2 fish";
+// while ((result = reg.exec(sampleString)) !== null) {
+//     console.dir(JSON.stringify(result))
+// };
 // '["1 cat","cat"]'
 // '["2 fish","fish"]'
-console.log('test7')
-var reg = new RegExp('([0-9]+ (cat|fish))','g'), sampleString="1 cat and 2 fish";
-while ((result = reg.exec(sampleString)) !== null){
-    console.dir(JSON.stringify(result))
-};
+
+// console.log('test7')
+// var reg = new RegExp('([0-9]+ (cat|fish))','g'), sampleString="1 cat and 2 fish";
+// while ((result = reg.exec(sampleString)) !== null){
+//     console.dir(JSON.stringify(result))
+// };
 // '["1 cat","1 cat","cat"]'
 // '["2 fish","2 fish","fish"]'
 
@@ -88,10 +89,19 @@ while ((result = reg.exec(sampleString)) !== null){
 
 
     const text = result.data;
-    
+    const splits = [];
     while ((result = regex1.exec(text)) !== null){
       console.dir(JSON.stringify(result))
-  };
+      const oneSplit = {
+        jump: Number(result[4] / result[5]),
+        year: Number(result [3]),
+        month: Number(result[2]),
+        day: Number(result[1]),
+      }
+      splits.push(oneSplit);
+
+    };
+    console.log (JSON.stringify(splits))
     // const found1 = [...text.matchAll(regex1)];
     // const found = regex1.matchAll (pattern);
     // console.log ('found ' + found1)
