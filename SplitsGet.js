0dc3141aac579edcc4a0aaf576b1c82539891c8d
Eli Shagam <eli.shagam@gmail.com>
2022-09-02 11:19:44 +0300

    modified:   SplitsGet.js log after reading file, Add split.length

--- SplitsGet.js
+++ SplitsGet.js
@@ -23,7 +23,7 @@ fs.readFile('splitsArray.txt', 'utf8', (err, data) => {
   else {
     var symbols ="";
     for (var i = 0; i < keys.length; i++)
-      symbols += keys[i] + '  ';
+      symbols += keys[i] + ' (' + splitsArray[keys[i]].length + ')  ';
     console.log (symbols)
   }
   // for (var i = 0; i < keys.length; i++)
