82a929e20712245b2a976692e1a883a0ee43eb7a
shagam <eli.shagam@gmail.com>
2024-03-05 08:49:48 +0200

    Add google search for chalange test	modified:   certbot.bat

--- certbot.bat
+++ certbot.bat
@@ -4,8 +4,12 @@
 
 sudo certbot --manual --preferred-challenges dns certonly -d dinagold.org 
 
+2024 Mar 4
+namecheap/mange/advanced/add a new record
 
-#_acme-challenge.dinagold.org.
+@REM https://toolbox.googleapps.com/apps/dig/#TXT/_acme-challenge.dinagold.org
 
+#_acme-challenge.dinagold.org
 
+@REM HTDNSNgIUZ9xRgvR_yu2t0L_IXcJR29VnkubPYq7rNo
  
