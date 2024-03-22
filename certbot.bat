591052ee3f1381422e0b2f8fa832a9816b126c2c
shagam <eli.shagam@gmail.com>
2024-03-22 07:42:36 +0200

    Change to dinagold.net	modified:   certbot.bat

--- certbot.bat
+++ certbot.bat
@@ -2,14 +2,15 @@
 #  renew certbot key
 #  add txt record _acme-challenge in namecheap 
 
-sudo certbot --manual --preferred-challenges dns certonly -d dinagold.org 
+sudo certbot --manual --preferred-challenges dns certonly -d dinagold.net 
+#sudo certbot --manual --preferred-challenges dns certonly -d dinagold.org 
 
-2024 Mar 4
-namecheap/mange/advanced/add a new record
+# 2024 Mar 4 namecheap/mange/advanced/add a new record
 
-@REM https://toolbox.googleapps.com/apps/dig/#TXT/_acme-challenge.dinagold.org
+# @REM https://toolbox.googleapps.com/apps/dig/#TXT/_acme-challenge.dinagold.net
+# @REM https://toolbox.googleapps.com/apps/dig/#TXT/_acme-challenge.dinagold.org
 
-#_acme-challenge.dinagold.org
+# @REM #_acme-challenge.dinagold.org
 
-@REM HTDNSNgIUZ9xRgvR_yu2t0L_IXcJR29VnkubPYq7rNo
+# @REM HTDNSNgIUZ9xRgvR_yu2t0L_IXcJR29VnkubPYq7rNo
  
