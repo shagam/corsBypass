5c4597ebd52ddb2d5cdce5e0f8cf5bf6166a6fe4
shagam <eli.shagam@gmail.com>
2024-04-05 11:14:58 +0200

    Partial Email.js still no email	modified:   Email.js

--- Email.js
+++ Email.js
@@ -1,17 +1,50 @@
-
+const nodeMailer = require('nodemailer')
 const {getDate} = require ('./Utils')
+
+
+async function main (name, email, html) {
+    const transporter = nodeMailer.createTransport({
+        // host: 'mail.openjavascript.info',
+        host: 'mail.dinagold.org',
+        port: 465, // SMTP
+        secure: true,
+        auth: {
+            // user: 'eli@mail.dinagold.org',
+            user: 'eli',
+            pass: 'm'
+        }
+    })
+    const info = await transporter.sendMail({
+        from: 'OpenJavaScript <j32111@gmail.com>',
+        to: 'eli.shagam@gmail.com',
+        subject: 'Contact Us',
+        html: html
+    })
+    console.log ("Message sent: " + info.messageId)
+}
+
+
 
 function email (app)  {
     // nowMili = Date.now();
 
+    
     app.get('/email', (req, res) => {
 
-        console.log (getDate(), 'Send email, name=', req.query.name, 'email=',
+        console.log (getDate(), 'email to be sent, name=', req.query.name, 'email=',
          req.query.email, 'message=', req.query.message)
+ 
+         const html = `
+         <h1> ${req.query.message} </h1>
+         `
 
-
+         main(req.query.name, req.query.email, html)
+         .catch(e => console.log('send fail', e))
+         
         res.send ('ok')
     })
 }
+
+
 
 module.exports = {email}
