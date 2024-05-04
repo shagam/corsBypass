aa33266761051897d3c911abda05ee8b9514cc23
shagam <eli.shagam@gmail.com>
2024-05-04 11:03:17 +0300

    Add diff (patial save)	modified:   Vix.js

--- Vix.js
+++ Vix.js
@@ -9,31 +9,44 @@ function vix (app) {
   const updateDate = getDate()
     // app.get('/holdingsSch', (req, res) => {
     app.get('/vix', (req, res) => {
-        const url = 'https://www.google.com/search?q=vix'
+      // https://go.cboe.com/l/77532/2021-10-13/bwkqfd
+      // https://www.marketwatch.com/investing/index/vix
+        
+      
+      // https://finance.yahoo.com/quote/%5EVIX/history
+  //<tr class="svelte-ewueuo"><td class="svelte-ewueuo">May 3, 2024</td> <td class="svelte-ewueuo">14.51</td> <td class="svelte-ewueuo">14.58</td> <td class="svelte-ewueuo">13.69</td> <td class="svelte-ewueuo">14.07</td> <td class="svelte-ewueuo">14.07</td> <td class="svelte-ewueuo">-</td> </tr>
+
+     
+        var url = 'https://www.google.com/search?q=vix'
+        // url = 'https://finance.yahoo.com/quote/%5EVIX/history'
+
         console.log ('vix,', url)
         axios.get (url)
         .then ((result) => {
           const text = result.data;
-          console.log ('response length=', text.length)
-
+          // console.log ('response length=', text.length)
           // fs.writeFile ('vix.txt', text, err => {
           //   if (err) {
           //     console.err('vix.txt write fail', err)
           //   }
           // })
-      
-          //<span jsname="vWLAgc" class="IsqQVc NprOob wT3VGc">14.87</span>
+          pattern = '"BNeawe iBp4i AP7Wnd">13.49 <span dir="ltr" class="rQMQod lB8g7">-1.19 (8.11%)</span>'
+          //<span jsname="vWLAgc" class="IsqQVc NprOob wT3VGc">14.87</span>"BNeawe iBp4i AP7Wnd">13.49 <span dir="ltr" class="rQMQod lB8g7">-1.19 (8.11%)</span>
           //<span jsname="vWLAgc" class="IsqQVc NprOob wT3VGc">13.82</span>
         // <span jsname="vWLAgc" class="IsqQVc NprOob wT3VGc">13.92</span>
           var pattern
           pattern = '"BNeawe iBp4i AP7Wnd">([0-9\\.]+) <span dir="ltr" class="rQMQod lB8g7">'
+
+       // pattern = '"BNeawe iBp4i AP7Wnd">13.49 <span dir="ltr" class="rQMQod lB8g7">-1.19 (8.11%)</span>'
+          pattern = '"BNeawe iBp4i AP7Wnd">([0-9\\.]+) <span dir="ltr" class="rQMQod lB8g7">([\\-]?[0-9\\.]+) \\(8.11%\\)</span>'
+
           // pattern = 'wT3VGc">([0-9\\.]+)</span>'
           var rx = new RegExp (pattern,'g');
           while ((rs = rx.exec(text)) !== null){
-            res.send(rs[1])
-            console.log('vix ', rs[1])
-                return
-            };
+            console.log('vix ', rs[1], '  ', rs[2], '  ', rs[3])
+            res.send(rs[1] + '   ' + rs[2] + '   ' + rs[3])
+            return
+          };
           const failTxt = 'fail, vix data not found' ;
           res.send (failTxt)
           console.log (failTxt)
@@ -41,8 +54,8 @@ function vix (app) {
         })
         .catch ((err) => {
           console.log(updateDate, err.message)
-          res.send('fail,' + err.message)
-          console.log( updateDate, 'fail, holding gail')
+          res.send('fail, ' + err.message)
+          console.log( updateDate, 'fail, holding fail')
         })    
       }
     )
