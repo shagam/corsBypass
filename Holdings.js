e1eb614aa038e65720ca90ebeef7761f38cdc84e
shagam <eli.shagam@gmail.com>
2024-05-10 13:00:03 +0300

    typo	modified:   Holdings.js

--- Holdings.js
+++ Holdings.js
@@ -178,7 +178,7 @@ function holdings (req, res, daysDelay, ignoreSaved) {
   .catch ((err) => {
     console.log(req.query.stock, updateDate, err.message)
     res.send('fail,' + err.message)
-    console.log(req.query.stock, updateDate, 'fail, holding gail')
+    console.log(req.query.stock, updateDate, 'fail, holding fail')
   })
 
 }
