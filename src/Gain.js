7a0df82fafd044f1c381c60219a815e9fac25d06
shagam <eli.shagam@gmail.com>
2024-08-17 15:24:29 +0300

    if ( ! 'year' in gainArray[sym])	modified:   src/Gain.js

--- src/Gain.js
+++ src/Gain.js
@@ -324,7 +324,7 @@ function gain (app)  {
             const keys = Object.keys(gainArray);
             console.log ('countBefore=', keys.length)
             keys.forEach ((sym) => {
-                if (typeof(gainArray[sym].year) === 'undefined' || gainArray[sym].year === null)
+                if ( ! 'year' in gainArray[sym])
                     console.log ('missing year')
                 if (LOG)
                     console.log (gainArray[sym])
