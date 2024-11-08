0f4739ce21f1f95f1c4d556cbbfba4cdeaf9f1f4
shagam <eli.shagam@gmail.com>
2024-11-08 19:54:20 +0200

    build futureArr still not saved	modified:   src/Futures.js

--- src/Futures.js
+++ src/Futures.js
@@ -12,10 +12,13 @@ var priceArray = {};   // saved one obj per stock
 
 function futures(app) {
 
+  var futureArr = {}
+
   const updateDate = getDateOnly ()
   app.get('/futures', (req, res) => {
 
     console.log ('\n\n', getDate(), 'futures', req.query)
+    const LOG = req.query.LOG
 
     var url = 'https://www.barchart.com/futures/quotes/' + req.query.stock
 
@@ -46,17 +49,25 @@ function futures(app) {
      // var pattern = 'init({"symbol":"NQZ24","symbolName":"Nasdaq 100 E-Mini","symbolType":2,"lastPrice":"21,237.50","priceChange":'
       var pattern = 'init({"symbol":"NQZ24","symbolName":"Nasdaq 100 E-Mini","symbolType":2,"lastPrice":"[0-9\\.,]","priceChange":'
       pattern = '"lastPrice\\\\":\\\\"([0-9\\.,]+)'
-      // pattern = '."lastPrice.":."([0-9\\.,]+)."'
-      // pattern = 'lastPrice\\":\\"([0-9\\.,]+)'
-      // pattern = '(lastPrice)\\\\"'
+
 
       var regex1 = new RegExp (pattern);
       var regExpResult = regex1.exec(choppedTxt)
+      const lastPrice = regExpResult[1]
 
-      console.log ('futures', req.query.stock, regExpResult[1])
+      console.log (getDateOnly(), 'futures', req.query.stock, lastPrice)
 
+      if (! futureArr[req.query.stock])
+        futureArr[req.query.stock] = {}
+      if (! futureArr[req.query.stock][getDateOnly ()] || req.query.ignoreSaved) {
+        futureArr[req.query.stock][getDateOnly ()] = lastPrice;
+
+        if (LOG)
+          console.log (futureArr)
+      }
+      
       const future = {
-        lastPrice: regExpResult[1],
+        lastPrice: lastPrice,
         sym: req.query.stock,
       }
       res.send (future)
