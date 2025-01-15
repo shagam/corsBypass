3080ea35033d014369cb70e7e3fdb3cdec22fc1f
shagam <eli.shagam@gmail.com>
2025-01-15 22:31:23 +0200

    check for fail, wrong exchange	modified:   src/LatestPrice.js

--- src/LatestPrice.js
+++ src/LatestPrice.js
@@ -9,8 +9,16 @@ function latestPrice (app) {
     console.log ('\n', getDate(), 'latestPrice', req.query)
     const LOG = req.query.LOG
     var url;
-    if (req.query.src === 'goog')
+    var pattern;
+    if (req.query.src === 'goog'){
         url = 'https://www.google.com/finance/quote/' + req.query.stock + ':NASDAQ'
+        pattern = 'data-currency-code="USD" data-last-price="([0-9.]+)'
+    }
+    else if (req.query.src === 'watch') {
+        url = 'https://www.nasdaq.com/market-activity/etf/' + req.query.stock
+            // <bg-quote class="value" field="Last" format="0,0.00" channel="/zigman2/quotes/208575548/composite,/zigman2/quotes/208575548/lastsale" session="pre">513.26</bg-quote>
+        pattern = '<bg-quote class="value" field="Last" format="0,0.00" channel="/zigman2/quotes/208575548/composite,/zigman2/quotes/208575548/lastsale" session="pre">([0-9.]+)</bg-quote>'
+    }
 
     console.log (url)
     axios.get (url)
@@ -20,16 +28,21 @@ function latestPrice (app) {
         console.log ('resp length', JSON.stringify (text).length)
 
     //   data-currency-code="USD" data-last-price="505.08" 
-        const pattern = 'data-currency-code="USD" data-last-price="([0-9.]+)'
+
         const rx = new RegExp (pattern,'g');
         const rs = rx.exec(text) 
 
-        const dat = {
-            price: rs[1],
-        } 
-        console.log ('response', dat)
-        res.send (dat)
-
+        if (rs) {
+            const dat = {
+                price: rs[1],
+            } 
+            console.log ('response', dat)
+            res.send (dat)
+        }
+        else {
+            console.log ('fail')
+            res.send ('fail')     
+        }
         })
     .catch ((err) => {
         console.log(req.query.stock, err.message)
