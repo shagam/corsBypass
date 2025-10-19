94a0d862888c3bc1c416eb06d6f74ce9bee1e3e6
shagam <eli.shagam@gmail.com>
2025-10-19 20:44:29 +0300

    correct image dounload 	modified:   src/Image.js

--- src/Image.js
+++ src/Image.js
@@ -2,17 +2,12 @@ const fs = require("fs")
 
 async function image (app) {
 
-    console.log ('image')
     app.get("/download", async function (request, result) {
 
-    console.log ('params=', request.params)
-    const img = request.params.img
-    // const image = await db.collection("../images")
-    //     .findOne({
-    //     name: name
-    //     })
-    if (true) {
-        let defaultImage = await fs.readFileSync("./downloads/QQQ_call_options.jpg")
+    console.log ('Download image, params=', request.query)
+
+      if (true) {
+        let defaultImage = await fs.readFileSync("./downloads/" + request.query.img)
         defaultImage = Buffer.from(defaultImage, "base64")
         
         result.writeHead(200, {
@@ -23,15 +18,8 @@ async function image (app) {
         return
     }
     
-      const imageContent = await fs.readFileSync(image.path)
-    
-      result.writeHead(200, {
-        "Content-Type": "image/jpg",
-        "Content-Length": imageContent.length
-      })
-      result.end(imageContent)
-      return
-    })
+    }
+  )
 }
 
   module.exports = {image}
