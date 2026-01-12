66a9a59d08da71be2aec1625e2681d339689ce27
shagam <eli.shagam@gmail.com>
2026-01-12 22:29:40 +0200

    count tokens per ip 	modified:   src/AiLog.js

--- src/AiLog.js
+++ src/AiLog.js
@@ -2,10 +2,40 @@ const fs = require ('fs')
 const axios = require('axios')
 const {getDate} = require ('./Utils')
 
-var tokenCount = {
-    deepSeek:5,
-    openAi: 5,
-    status: 'ok'
+const print_textFiles = false
+var tokenCountArray = {};   // saved one obj per stock
+
+// read tokenCount from local file once on startup
+fs.readFile('txt/tokenCountArray.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error (err)
+    return;
+  }
+  tokenCountArray = JSON.parse(data);
+  const keys = Object.keys(tokenCountArray);
+  console.log('\n', getDate(), 'txt/tokenCountArray.txt  read, count=', keys.length)
+  if (print_textFiles) {
+    for (var i = 0; i < keys.length; i++)
+      console.log (JSON.stringify (tokenCountArray[keys[i]]))
+  }
+  else {
+      var symbols = "";
+      for (var i = 0; i < keys.length; i++)
+        symbols += keys[i] + '  '
+      console.log(symbols)
+  }
+});
+
+function tokenCountFlush () {
+  if (Object.keys(tokenCountArray).length === 0) // avoid write of empty
+    return;
+  fs.writeFile ('txt/tokenCountArray.txt', JSON.stringify (tokenCountArray), err => {
+    if (err) {
+      console.log (getDate(), 'txt/tokenCountArray.txt write fail', err)
+    }
+    else
+      console.log (getDate(), 'txt/tokenCountArray.txt sym count:', Object.keys(tokenCountArray).length)
+  })
 }
 
 function aiLog (app) {
@@ -13,19 +43,32 @@ function aiLog (app) {
 //   console.log ('aiLog')
 
   app.get('/ai', (req, res) => {
-    // price(req, res)
+
     console.log ('\n\n' + getDate(), 'aiLog', req.query)
 
-    const stock = req.query.stock
+    const ip = req.query.ip
+    if (! tokenCountArray[ip]) {
+        tokenCountArray[ip] = {openAi: 0,   deepSeek: 0,}
+    }
+
+
     if (req.query.brand==='OpenAI')
-        tokenCount.openAi += Number(req.query.tokenCount)
+        tokenCountArray[ip].openAi += Number(req.query.tokenCount)
     else if (req.query.brand==='DeepSeek')
-        tokenCount.deepSeek += Number(req.query.tokenCount)
+        tokenCountArray[ip].deepSeek += Number(req.query.tokenCount)
     else {
         res.send ({status: 'fail', err: ' wrong brand'})
+        return;
     }
- 
-    console.log (tokenCount)
+    tokenCountFlush () 
+
+    var tokenCount = {
+        openAi: tokenCountArray[ip].openAi,
+        deepSeek: tokenCountArray[ip].deepSeek, 
+        status: 'ok'
+    }
+
+    console.log ('AI returned tokenCounters', tokenCount)
     res.send (tokenCount)
 
 
