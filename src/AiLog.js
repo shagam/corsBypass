69c1908f9395faa2db6a40b95f8fc0f52e8123ab
shagam <eli.shagam@gmail.com>
2026-01-12 21:45:00 +0200

    return object with attributes of counters	modified:   src/AiLog.js

--- src/AiLog.js
+++ src/AiLog.js
@@ -2,17 +2,34 @@ const fs = require ('fs')
 const axios = require('axios')
 const {getDate} = require ('./Utils')
 
+var tokenCount = {
+    deepSeek:5,
+    openAi: 5,
+    status: 'ok'
+}
+
 function aiLog (app) {
   nowMili = Date.now();
-  console.log ('aiLog')
+//   console.log ('aiLog')
 
   app.get('/ai', (req, res) => {
     // price(req, res)
     console.log ('\n\n' + getDate(), 'aiLog', req.query)
-    // console.log (getDate(), req.query.stock, req.query.mon, req.query.day, req.query.year)
+
     const stock = req.query.stock
-    res.send ('ok')
-      return;   
+    if (req.query.brand==='OpenAI')
+        tokenCount.openAi += Number(req.query.tokenCount)
+    else if (req.query.brand==='DeepSeek')
+        tokenCount.deepSeek += Number(req.query.tokenCount)
+    else {
+        res.send ({status: 'fail', err: ' wrong brand'})
+    }
+ 
+    console.log (tokenCount)
+    res.send (tokenCount)
+
+
+    return;   
   })
 }
 
