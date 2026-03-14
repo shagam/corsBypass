0e3f210e47408f5f2b0e89e301e732629dba0b52
shagam <eli.shagam@gmail.com>
2026-03-14 23:04:31 +0200

    polymarket first working  version

--- src/Polymarket.js
+++ src/Polymarket.js
@@ -9,21 +9,22 @@ function polymarket (app)  {
     console.log ('\n\n\n', getDate(), 'polymarket prepare')
 
     app.get('/polymarket', async (req, res) => {
-        console.log ('polymarket query params', req.query)
+        console.log ('polymarket query params', req.query) 
    
       // var url = "https://gamma-api.polymarket.com/markets"
-      var url = "https://polymarket.com/event/ndx-above-dec-2026"
-      url = "https://gamma-api.polymarket.com/events?slug=ndx-above-dec-2026&limit=1"
-      url = "https://gamma-api.polymarket.com/events?slug=ndx-above-dec-2026&limit=1"
+      // var url = "https://polymarket.com/event/ndx-above-dec-2026"
+      var url = "https://gamma-api.polymarket.com/events?slug=ndx-above-dec-2026&limit=1"
 
-    // if (req.query.url)
+
+      if (req.query.url)
         url = req.query.url
 
 
      try {
 
       // url = "https://gamma-api.polymarket.com/markets?limit=2"
-      const resp = await axios.get ("https://gamma-api.polymarket.com/events?slug=ndx-above-dec-2026&limit=1",
+      const resp = await axios.get (url,
+        // "https://gamma-api.polymarket.com/events?slug=ndx-above-dec-2026&limit=1",
         {
           headers: {
             "Accept": "application/json",
@@ -45,14 +46,25 @@ function polymarket (app)  {
           return;
         }
 
-    //  const markets = event.markets.map(market => ({
-    //       question: market.question,
-    //       yesPrice: market.outcomePrices[0],
-    //       noPrice: market.outcomePrices[1],
-    //       volume: market.volume
-    //   }));
+        const event = data[0]
+        // const markets = event.markets
+        // console.log('\n\n\n', markets.length)
+        
+        const markets = event.markets.map(market => ({
+            question: market.question,
+            slug: market.slug,
+            yesPrice: JSON.parse(market.outcomePrices)[0],
+            //yesPrice: market.outcomePrices[0],
+            // noPrice: market.outcomePrices[1],
+            //volume: market.volume
+        }));
+
+        // console.log ('\n\n', event.markets)
+        // console.log (markets)
+
+
         const response = {
-          data: resp.data.length,
+          data: markets,
         }
         res.send (response)
         console.log ('response', response)
@@ -61,16 +73,7 @@ function polymarket (app)  {
 
       
    
-    // console.log(markets);
-    //     console.log ('response:', result.data)
-    //     const response = {
-    //     status: 'fail',
-    //     data: result.data
-    //   }
-    //     res.send (response)
-    //     return 'ok'
-    //  })
-      
+       
 
       
       // const response = axios.get(POLYMARKET_API, {
@@ -81,26 +84,13 @@ function polymarket (app)  {
       // }
       // });
 
-      // console.log ('response:', response)
-      // const markets = response.data;
-
-
       // const filtered = markets.filter(m =>
       //   m.question.toLowerCase().includes("nasdaq") ||
       //   m.question.toLowerCase().includes("ndx") ||
       //   m.question.toLowerCase().includes("qqq")
       // );
 
-      // const result = filtered.map(m => ({
-      //   question: m.question,
-      //   slug: m.slug,
-      //   yesPrice: m.outcomePrices?.[0],
-      //   noPrice: m.outcomePrices?.[1],
-      //   volume: m.volume
-      // }));
-
-      // res.json(response);
-
+      
   } catch (err) {
       console.error(err);
       res.status(500).json({ error: "Failed to fetch polymarket data" });
@@ -109,14 +99,9 @@ function polymarket (app)  {
           message: err.message,
           code:  err.code 
       }
-            // console.log (result)
-      // console.log (JSON.stringify(err))
-      // res.send (result)
-      return 'fail'
-      }
+    }
+  })
 
-    })
-
-  }
+}
 
 module.exports = {polymarket}
